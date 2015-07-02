b71fac585de0f45feea2bfce531fb3466cb121c0
Bhanu Agarwal <bhanu.agarwal.13@gmail.com>
2015-07-03 00:58:44 +0530

    Problem 3 Done

--- prob3.py
+++ prob3.py
@@ -1,5 +1,15 @@
-n = 600851475143
+import math
 
-sqrt = n ** (1/2)
+def largest_prime(n):
+	factors = [1]
+	sqrt = int(math.sqrt(n))
+	print sqrt
 
-for
+	for i in range(2,sqrt):
+		if(n % i == 0):
+			factors.append(i)
+			n = int(n/i)
+
+	return factors[-1]
+
+print largest_prime(600851475143)
